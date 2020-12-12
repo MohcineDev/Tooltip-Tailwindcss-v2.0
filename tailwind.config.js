ad75d0fe6bf49339c7e74389775e17602c0f48bd
MohcineDev <mouhcinedev@gmail.com>
2020-12-12 10:54:37 +0000

    Create files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./index.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+
+  variants: {
+    extend: {},
+    display: ['group-hover'],
+    visibility : ['group-hover']
+  },
+  plugins: [],
+}
